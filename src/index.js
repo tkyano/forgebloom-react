45c874aa9b5c2b3e99ea63b15c3b14d27d2ede71
Timothy Kyano <tdkyano05@gmail.com>
2025-10-26 16:57:37 -0400

    f

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import CreateDeck from './pages/CreateDeck';
 
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PIBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route index element={<Home />}/>
